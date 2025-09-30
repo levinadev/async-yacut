f6da1068241c0a8a63a63580cf4a774b5d9aa4f0
levinadevx <anna_levina@internet.ru>
2025-09-30 19:00:22 +0300

    fix: исправлено по пеп8

--- constants.py
+++ constants.py
@@ -1,12 +1,10 @@
 import string
 import re
 
-
 # Константы для короткой ссылки (custom_id)
 CUSTOM_ID_CHARACTERS = string.ascii_letters + string.digits
-CUSTOM_ID_ALLOWED_PATTERN = re.compile(rf"^[{re.escape(CUSTOM_ID_CHARACTERS)}]+$")
-
-
+pattern = rf"^[{re.escape(CUSTOM_ID_CHARACTERS)}]+$"
+CUSTOM_ID_ALLOWED_PATTERN = re.compile(pattern)
 CUSTOM_ID_RESERVED = ["files", "api", "admin"]
 CUSTOM_ID_MIN_LENGTH = 1
 CUSTOM_ID_MAX_LENGTH = 16
