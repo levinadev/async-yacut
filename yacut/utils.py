bc4e526ea3c19f12588e9e1433653ce33e70b949
levinadevx <anna_levina@internet.ru>
2025-09-30 15:48:20 +0300

    fix: вынесены константы

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,8 +1,9 @@
 import random
 import string
 
+DEFAULT_SHORT_ID_LENGTH = 6
+SHORT_ID_CHARACTERS = string.ascii_letters + string.digits
 
-def get_unique_short_id(length=6):
+def get_unique_short_id(length=DEFAULT_SHORT_ID_LENGTH):
     """Генерация случайного короткого идентификатора."""
-    characters = string.ascii_letters + string.digits
-    return "".join(random.choices(characters, k=length))
+    return "".join(random.choices(SHORT_ID_CHARACTERS, k=length))
